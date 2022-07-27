b54a2ccb9058d571f9266581ba526f9ee6ecc6e3
orindholt <u75087@rts-365.dk>
2022-07-27 16:13:36 +0200

    Fix swiper bug and add several correction to markup

---
+++ js/swiper.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", () => {
+	const deviceType = () => {
+		const ua = navigator.userAgent;
+		if (/(tablet|ipad|playbook|silk)|(android(?!.*mobi))/i.test(ua)) {
+			return "tablet";
+		} else if (
+			/Mobile|Android|iP(hone|od)|IEMobile|BlackBerry|Kindle|Silk-Accelerated|(hpw|web)OS|Opera M(obi|ini)/.test(
+				ua
+			)
+		) {
+			return "mobile";
+		}
+		return "desktop";
+	};
+	console.log(deviceType());
+	// Sliders
+	if (deviceType() === "mobile") {
+		document.querySelectorAll(".splide").forEach(el => {
+			new Splide(el, {
+				type: "loop",
+				perPage: deviceType() === "desktop" ? 2 : 1,
+				arrows: false,
+				pagination: false,
+				gap: "1rem",
+				fixedWidth: deviceType() === "desktop" ? "44%" : "75%",
+			}).mount();
+		});
+	}
+});
