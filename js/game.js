0d99bdf2ded4e7cd2d0f11fd57e5cdc8bac1b55c
orindholt <U75087@rts-365.dk>
2022-07-28 12:18:28 +0200

    Add init changes to game page

--- js/game.js
+++ js/game.js
@@ -1,13 +1,6 @@
 $(document).ready(() => {
-	$("[data-id='start-button']").click(function () {
-		$("[data-id='game-active']").slideToggle();
-		$("[data-id='start-button']").slideToggle();
-	});
 	$("[data-id='leaderboard-toggle-button']").click(function () {
 		$("[data-id='leaderboard-overlay']").fadeToggle();
-	});
-	$("[data-id='fullscreen-toggle-button']").click(function () {
-		$("[data-id='fullscreen-demo']").toggleClass("fullscreen");
 	});
 	$("[data-id='notification-toggle-button']").click(function () {
 		$(this).closest("[data-id='notification']").hide();
