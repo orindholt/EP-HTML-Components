83ca9e043df2e080f4488a67eb4be801e1af7d6c
orindholt <U75087@rts-365.dk>
2022-10-04 19:03:41 +0200

    Add bigger img, and min height to offer cards

--- js/swiper.js
+++ js/swiper.js
@@ -1,21 +1,4 @@
 document.addEventListener("DOMContentLoaded", () => {
-	/* const deviceType = () => {
-		const ua = navigator.userAgent;
-		if (/(tablet|ipad|playbook|silk)|(android(?!.*mobi))/i.test(ua)) {
-			return "tablet";
-		} else if (
-			/Mobile|Android|iP(hone|od)|IEMobile|BlackBerry|Kindle|Silk-Accelerated|(hpw|web)OS|Opera M(obi|ini)/.test(
-				ua
-			)
-		) {
-			return "mobile";
-		}
-		return "desktop";
-	};
-	console.log(deviceType()); */
-	// Sliders
-	/* if (!deviceType() === "mobile") return; */
-
 	document.querySelectorAll(".splide").forEach(el => {
 		new Splide(el, {
 			arrows: false,
